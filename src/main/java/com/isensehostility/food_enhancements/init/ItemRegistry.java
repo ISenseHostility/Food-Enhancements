27fd80bd67db79a6e36c6b9bc609993cd5e9cfe8
ISenseHostility <yaboijarno@gmail.com>
2022-09-30 23:47:15 +0200

    Added skewers to registry

--- src/main/java/com/isensehostility/food_enhancements/init/ItemRegistry.java
+++ src/main/java/com/isensehostility/food_enhancements/init/ItemRegistry.java
@@ -91,4 +91,16 @@ public class ItemRegistry {
     public static final RegistryObject<CaramelChocolate> CARAMEL_CHOCOLATE = ITEMS.register("caramel_chocolate", CaramelChocolate::new);
     public static final RegistryObject<CaramelGlowBerries> CARAMEL_GLOW_BERRIES = ITEMS.register("caramel_glow_berries", CaramelGlowBerries::new);
     public static final RegistryObject<CaramelSweetBerries> CARAMEL_SWEET_BERRIES = ITEMS.register("caramel_sweet_berries", CaramelSweetBerries::new);
+
+    public static final RegistryObject<CookedChickenSkewer> COOKED_CHICKEN_SKEWER = ITEMS.register("cooked_chicken_skewer", CookedChickenSkewer::new);
+    public static final RegistryObject<CookedPorkchopSkewer> COOKED_PORKCHOP_SKEWER = ITEMS.register("cooked_porkchop_skewer", CookedPorkchopSkewer::new);
+    public static final RegistryObject<CookedMuttonSkewer> COOKED_MUTTON_SKEWER = ITEMS.register("cooked_mutton_skewer", CookedMuttonSkewer::new);
+    public static final RegistryObject<SteakSkewer> STEAK_SKEWER = ITEMS.register("steak_skewer", SteakSkewer::new);
+    public static final RegistryObject<CookedRabbitSkewer> COOKED_RABBIT_SKEWER = ITEMS.register("cooked_rabbit_skewer", CookedRabbitSkewer::new);
+    public static final RegistryObject<HoneyRoastedChickenSkewer> HONEY_ROASTED_CHICKEN_SKEWER = ITEMS.register("honey_roasted_chicken_skewer", HoneyRoastedChickenSkewer::new);
+    public static final RegistryObject<MixedGrillSkewer> MIXED_GRILL_SKEWER = ITEMS.register("mixed_grill_skewer", MixedGrillSkewer::new);
+    public static final RegistryObject<SeasonedPorkSkewer> SEASONED_PORK_SKEWER = ITEMS.register("seasoned_pork_skewer", SeasonedPorkSkewer::new);
+    public static final RegistryObject<SquidTentacleSkewer> SQUID_TENTACLE_SKEWER = ITEMS.register("squid_tentacle_skewer", SquidTentacleSkewer::new);
+    public static final RegistryObject<SpiderLegSkewer> SPIDER_LEG_SKEWER = ITEMS.register("spider_leg_skewer", SpiderLegSkewer::new);
+    public static final RegistryObject<CaveSpiderLegSkewer> CAVE_SPIDER_LEG_SKEWER = ITEMS.register("cave_spider_leg_skewer", CaveSpiderLegSkewer::new);
 }
