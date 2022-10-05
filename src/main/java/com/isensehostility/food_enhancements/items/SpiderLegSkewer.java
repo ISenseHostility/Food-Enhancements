9e53b41584009ea08893658f26118384ac5813d0
ISenseHostility <yaboijarno@gmail.com>
2022-10-06 01:29:08 +0200

    fixed missing skewer recipes

--- src/main/java/com/isensehostility/food_enhancements/items/SpiderLegSkewer.java
+++ src/main/java/com/isensehostility/food_enhancements/items/SpiderLegSkewer.java
@@ -4,15 +4,15 @@ import com.isensehostility.food_enhancements.FoodEnhancements;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 
-public class MixedGrillSkewer extends Item {
+public class SpiderLegSkewer extends Item {
 
-    public MixedGrillSkewer() {
+    public SpiderLegSkewer() {
         super(new Properties()
                 .tab(FoodEnhancements.TAB)
                 .food(new FoodProperties.Builder()
-                        .nutrition(13)
-                        .saturationMod(0.9F)
+                        .nutrition(8)
                         .meat()
+                        .saturationMod(0.6F)
                         .build())
 
         );
